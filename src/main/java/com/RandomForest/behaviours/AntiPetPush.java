c399708ce461c6509a23715477ac81d40737de16
Alex <alexandros.vallatos@hotmail.com>
2023-12-13 21:32:09 +0200

    Replaced & operator with &&. Improved documentation. Used i18nAPI instead of hard-coded strings.

--- src/main/java/com/RandomForest/behaviours/AntiPetPush.java
+++ src/main/java/com/RandomForest/behaviours/AntiPetPush.java
@@ -19,15 +19,18 @@ import eu.darkbot.api.extensions.Configurable;
 import eu.darkbot.api.extensions.Feature;
 import eu.darkbot.api.managers.BoosterAPI;
 import eu.darkbot.api.managers.BotAPI;
+import eu.darkbot.api.managers.I18nAPI;
 import eu.darkbot.api.managers.GameLogAPI.LogMessageEvent;
 import eu.darkbot.shared.legacy.LegacyModuleAPI;
 
-@Feature(name = "AntiPetPush", description = "Does a break when you are being pushed by pet")
+@Feature(name = "AntiPetPush", description = "Performs a break after you have been pushed by pet kills.")
 public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush.Config> {
 
     private final BotAPI bot;
     private final LegacyModuleAPI legacyModules;
     private final BoosterAPI booster;
+    private final I18nAPI i18n;
+
     private Config config;
     private final Pattern pattern;
     private final Pattern pattern2;
@@ -41,15 +44,24 @@ public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush
 
     @Configuration("anti_pet_push.config")
     public static class Config {
+        // Talent cruelty that gives up to 25% honor boost
         public @Number(min = 0, max = 25) int CRUELTY = 0;
+        // Talent tactics that gives up to 12% exp boost
         public @Number(min = 0, max = 12) int TACTICS = 0;
+
+        // honor and exp modules
         public @Number(min = 0, max = 50) int HONOR_MODULES = 0;
         public @Number(min = 0, max = 50) int EXP_MODULES = 0;
 
+        // push plugin setting for behaviour
         public @Number(min = -1, max = 300) Integer KILL_PAUSE_TIME = -1;
         public @Number(min = 1, max = 10) Integer MAX_KILLS = 5;
+
+        // kill count. (Doesn't work properly)
         @Readonly
         public @Number Integer KILL_COUNT = 0;
+
+        // If true then plugin considers pattern 2
         public boolean CHECK_FOR_WEAK_PLAYER_MESSAGE = false;
     }
 
@@ -58,48 +70,80 @@ public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush
         this.config = config.getValue();
     }
 
-    public AntiPetPush(BotAPI bot, LegacyModuleAPI legacyModules, BoosterAPI booster) {
+    public AntiPetPush(BotAPI bot, LegacyModuleAPI legacyModules, BoosterAPI booster, I18nAPI i18n) {
+        // APIs
         this.bot = bot;
         this.legacyModules = legacyModules;
-        this.pattern = Pattern.compile("You received (\\S+) (honor points|EP).");
-        this.pattern2 = Pattern.compile("You get less reward from (\\S+), he is too weak for you!");
+        this.booster = booster;
+        this.i18n = i18n;
+
+        // regex patterns
+        this.pattern = Pattern.compile(i18n.get("anti_pet_push.honor_exp_pattern"));
+        this.pattern2 = Pattern.compile(i18n.get("anti_pet_push.weak_player_pattern"));
+
+        // kill counter
         this.counter = 0;
+
+        // boolean that is true if pattern is detected
         this.found = false;
+
+        // variables that hold exp and honor detected
         this.exp = 0;
         this.honor = 0;
-        this.booster = booster;
+
+        // running boosters gotten from boosterAPI
         this.exp_boost = 0.0;
         this.honor_boost = 0.0;
+
+        // boolean that is true if pattern 2 is recognized
         this.isWeakPlayerMessage = false;
     }
 
+    // this function is called every time a new log message appears in game
     @EventHandler
     public void onLogReceived(LogMessageEvent ev) throws IOException {
+        // This function looks for 2 consecutive detections of pattern 1.
+        // Then calculates the ratio of exp/honor to distinguish a npc kill
+        // from a ship kill. (npc's ratio is 200 and ship's ratio is 100)
 
         if (pattern.matcher(ev.getMessage()).matches()) {
+            // If pattern 1 is detected, find honor|exp value from string and save it
             if (ev.getMessage().split(" ")[3].equals("honor")) {
                 honor = Integer.valueOf(ev.getMessage().split(" ")[2].replace(",", ""));
             } else {
                 exp = Integer.valueOf(ev.getMessage().split(" ")[2].replace(",", ""));
             }
 
+            // If found == true then this is the second consecutive recognition of pattern
+            // 1.
+            // (When an entity is killed, honor and exp rewards goes one after the other)
             if (found) {
+
+                // Get exp and honor booster ammount from boosterAPI. This doesn't include the
+                // buffs
+                // from modules, talents etc.
                 exp_boost = booster.getBoosters().stream().filter(bs -> bs.getName().equals("Experience"))
                         .collect(Collectors.toList()).get(0).getAmount();
                 honor_boost = booster.getBoosters().stream().filter(bs -> bs.getName().equals("Honor"))
                         .collect(Collectors.toList()).get(0).getAmount();
 
+                // Calculate the initial values of exp and honor. The categories of buffs are
+                // booster buffs, module buffs, talent buffs, formation buffs. Even though the
+                // total buff on each category is additive, the overall boost is cumulative.
                 exp = (int) (exp * 100 / (100 + exp_boost) * 100 / (100 + config.TACTICS)
                         * 100 / (100 + config.EXP_MODULES));
                 honor = (int) (honor * 100 / (100 + honor_boost) * 100 / (100 + config.CRUELTY)
                         * 100 / (100 + config.HONOR_MODULES));
 
-                if (honor == 0 || Math.abs((exp / honor) - 100) < 50) {
+                // If honor == 0 (phoenix gives 0 honor) or ratio is closer to 100 than to 200
+                // increade kill counter and reset found boolean.
+                if (honor == 0 || exp / honor - 150 < 0) {
                     counter += 1;
                     config.KILL_COUNT = counter;
                     found = false;
                 }
 
+                // This is for testing purposes.
                 Path path = Paths.get("test.txt");
                 Files.write(path,
                         ("Exp booster : " + exp_boost + " , Honor booster : " + honor_boost + " , exp : " + exp
@@ -107,20 +151,29 @@ public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush
                                 + " , modules : " + config.HONOR_MODULES + "\n").getBytes(),
                         StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             } else {
+                // This runs when it's the first time that pattern 1 is recognized.
                 found = true;
             }
         } else {
+            // This runs when log message didn't match with pattern 1
             found = false;
         }
 
-        if (pattern2.matcher(ev.getMessage()).matches() & config.CHECK_FOR_WEAK_PLAYER_MESSAGE) {
+        // If pattern 2 is recognized, set isWeakPlayerMessage boolean to true.
+        if (config.CHECK_FOR_WEAK_PLAYER_MESSAGE && pattern2.matcher(ev.getMessage()).matches()) {
             isWeakPlayerMessage = true;
         }
 
     }
 
+    // This function is called on every tick
     @Override
     public void onTickBehavior() {
+
+        // This function checks if kill counter >= max_kills or if pattern 2
+        // is detected and stops for pause_time minutes.
+        // Both max_kills and pause_time has been set up by the user.
+
         if (counter >= config.MAX_KILLS) {
             System.out.println("Pausing bot" +
                     (config.KILL_PAUSE_TIME > 0 ? " for " + config.KILL_PAUSE_TIME + " minutes" : "") +
@@ -128,7 +181,8 @@ public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush
 
             Long pauseMillis = config.KILL_PAUSE_TIME > 0 ? (long) config.KILL_PAUSE_TIME * 60 * 1000 : null;
             bot.setModule(
-                    legacyModules.getDisconnectModule(pauseMillis, "You Killed " + config.MAX_KILLS + " players"));
+                    legacyModules.getDisconnectModule(pauseMillis,
+                            i18n.get("anti_pet_push.disconnect.reason.kills", config.MAX_KILLS)));
             counter = 0;
             isWeakPlayerMessage = false;
         }
@@ -136,7 +190,8 @@ public class AntiPetPush implements Behavior, Listener, Configurable<AntiPetPush
         if (isWeakPlayerMessage) {
             Long pauseMillis = config.KILL_PAUSE_TIME > 0 ? (long) config.KILL_PAUSE_TIME * 60 * 1000 : null;
             bot.setModule(
-                    legacyModules.getDisconnectModule(pauseMillis, "Weak player message detected"));
+                    legacyModules.getDisconnectModule(pauseMillis,
+                            i18n.get("anti_pet_push.disconnect.reason.weak_player_message")));
             counter = 0;
             isWeakPlayerMessage = false;
         }
--- src/main/java/com/RandomForest/behaviours/BeheChanger.java
+++ src/main/java/com/RandomForest/behaviours/BeheChanger.java
@@ -27,6 +27,7 @@ import eu.darkbot.api.managers.StarSystemAPI;
 @Feature(name = "BeheChanger", description = "changes map when behe is destroyed")
 public class BeheChanger implements Behavior, Configurable<BeheChanger.Config> {
 
+    // API
     protected final PluginAPI api;
     protected final BotAPI bot;
     protected final HeroAPI hero;
@@ -36,24 +37,39 @@ public class BeheChanger implements Behavior, Configurable<BeheChanger.Config> {
     protected final RepairAPI repair;
     protected final HeroItemsAPI items;
 
+    // Ids of bl maps
     protected final Integer bl_1;
     protected final Integer bl_2;
     protected final Integer bl_3;
+
+    // Working map and npcTable fetched from bot
     protected final ConfigSetting<Integer> workingMap;
     protected final ConfigSetting<Map<String, NpcInfo>> npcTable;
+
+    // Behemoth info found on nptTable
     protected final NpcInfo beheInfo;
+
+    // True if behemoth is on map
     protected boolean isBeheOnMap;
+    // Ture if behemoth is near player
     protected boolean isBeheNear;
+
+    // maps that store the order of the change
     protected final Map<Integer, Integer> map;
     protected final Map<Integer, Integer> reversedMap;
+
+    // Variable that stores plugin config settings
     private Config config;
+
+    // Collection of ships around player
     private final Collection<? extends Ship> ships;
 
     @Configuration("behe_changer.config")
     public static class Config {
+        // if true then the order is reversed
         public boolean REVERSED = false;
+        // if true then use emp when enemy draws
         public boolean USE_EMP_ON_DRAW = false;
-
     }
 
     @Override
@@ -103,6 +119,7 @@ public class BeheChanger implements Behavior, Configurable<BeheChanger.Config> {
     }
 
     public void tickDrawFire() {
+        // This function uses emp if enemies draw. Emp must be in bar.
         if (config.USE_EMP_ON_DRAW) {
             for (Ship ship : ships) {
                 if (!ship.getEntityInfo().isEnemy() || !ship.hasEffect(EntityEffect.DRAW_FIRE)
@@ -114,37 +131,46 @@ public class BeheChanger implements Behavior, Configurable<BeheChanger.Config> {
     }
 
     public void tickCheckBehe() {
+        // This function checks for any disappearance of behemoth. It uses both
+        // isBeheNear and isBeheOnMap for a combination of speed and robustness.
+
         if (!repair.isDestroyed()) {
             boolean beheOnMap = checkBehe();
             boolean beheNear = checkBeheNear();
 
-            if (!isBeheNear & beheNear) {
+            if (!isBeheNear && beheNear) {
                 isBeheNear = beheNear;
-            } else if (isBeheNear & !beheNear) {
+            } else if (isBeheNear && !beheNear) {
                 isBeheNear = beheNear;
                 rotateMap();
             }
-            if(pet.isActive()){
-                if (!isBeheOnMap & beheOnMap) {
+            if (pet.isActive()) {
+                if (!isBeheOnMap && beheOnMap) {
                     isBeheOnMap = beheOnMap;
-                } else if (isBeheOnMap & !beheOnMap) {
+                } else if (isBeheOnMap && !beheOnMap) {
                     isBeheOnMap = beheOnMap;
                     rotateMap();
                 }
             }
-            
+
         }
     }
 
     public boolean checkBehe() {
+        // This function returns true if behemoth is on map. It gets pet's locator npc
+        // list from petAPI and checks if behe is included.
+        // This way bot can detect if behe is dead even when player is not near.
+
         return pet.getLocatorNpcs().stream().anyMatch(info -> info == beheInfo);
     }
 
     public boolean checkBeheNear() {
+        // This function returns true if behemoth is near player using EntitiesAPI.
         return entities.getNpcs().stream().map(Npc::getInfo).anyMatch(info -> info == beheInfo);
     }
 
     public void rotateMap() {
+        // Performs a map rotation by changin the working map.
         if (config.REVERSED) {
             workingMap.setValue(reversedMap.get(hero.getMap().getId()));
         } else {
